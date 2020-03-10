0cbe8a7ce25dec14f3386dbab6011ddc85984ad4
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-03-10 17:24:14 +0100

    Adding Joke controller

---
+++ src/main/java/rysich/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package rysich.springframework.joke.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import rysich.springframework.joke.services.JokeService;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
